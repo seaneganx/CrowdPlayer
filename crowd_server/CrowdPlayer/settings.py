d2dedf09194212c08fdb901f1df135cd3d93a149
Sean Egan <seaneganx@gmail.com>
2016-12-04 16:32:55 -0600

    created and configured MySQL database for the project

--- crowd_server/CrowdPlayer/settings.py
+++ crowd_server/CrowdPlayer/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'CrowdPlayer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crowd_player_db',
+        'USER': 'crowd_admin',
+        'HOST': 'localhost',
+        'POST': '',
     }
 }
 
