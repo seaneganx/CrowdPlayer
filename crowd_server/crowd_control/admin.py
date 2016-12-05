d1e4c07e519b53cb8132039fef73fd71f17db35e
Sean Egan <seaneganx@gmail.com>
2016-12-04 23:21:55 -0600

    Wrote database models and enabled adminCP access to each table

--- crowd_server/crowd_control/admin.py
+++ crowd_server/crowd_control/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from . models import Host, Room, Track
+
 # Register your models here.
+admin.site.register(Host)
+admin.site.register(Room)
+admin.site.register(Track)
--- crowd_server/crowd_control/models.py
+++ crowd_server/crowd_control/models.py
@@ -1,5 +1,88 @@
-from __future__ import unicode_literals
-
 from django.db import models
+from django.db.models import Min, Max
+
+from django.utils import timezone
 
 # Create your models here.
+class Host(models.Model):
+
+	# every host is attached to a spotify account
+	spotify_id = models.CharField(
+		'Spotify user ID',
+		max_length=64
+	)
+
+	# other host information
+	reg_date = models.DateTimeField(
+		'Registration Date',
+		default=timezone.now
+	)
+
+	def __str__(self):
+		return self.spotify_id
+
+class Room(models.Model):
+
+	# every room is attached to a host
+	host = models.OneToOneField(
+		Host,
+		on_delete=models.CASCADE
+	)
+
+	# other room information
+	creation_date = models.DateTimeField(
+		'Creation Date',
+		default=timezone.now
+	)
+
+	def get_track_queue(self):
+
+		# sort the tracks by vote count, with oldest track ID at the top
+		return self.track_set.order_by('-vote_count', 'id')
+
+	def __str__(self):
+		return "{host}'s Room".format(
+			host=self.host.spotify_id
+		)
+
+class Track(models.Model):
+
+	# every track needs to exist inside a room
+	room = models.ForeignKey(
+		Room,
+		on_delete=models.CASCADE
+	)
+
+	# other track information
+	spotify_id = models.CharField(
+		'Spotify track ID',
+		max_length=64
+	)
+
+	artist_name = models.CharField(
+		'Artist Name',
+		max_length=64
+	)
+	track_name = models.CharField(
+		'Track Name',
+		max_length=64
+	)
+	album_name = models.CharField(
+		'Album Name',
+		max_length=64
+	)
+	track_length_ms = models.IntegerField(
+		'Track Length (ms)'
+	)
+
+	vote_count = models.IntegerField(
+		'Vote Count',
+		default=0
+	)
+
+	def __str__(self):
+		return "{artist} - {track} ({votes})".format(
+			artist=self.artist_name,
+			track=self.track_name,
+			votes=self.vote_count
+		)
