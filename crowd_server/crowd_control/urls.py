752916e192c7dbf3fae7b642296f5369feea939d
Sean Egan <seaneganx@gmail.com>
2016-12-27 01:07:26 -0600

    created empty skeleton (501's everywhere) for the API

--- crowd_server/crowd_control/urls.py
+++ crowd_server/crowd_control/urls.py
@@ -2,6 +2,23 @@ from django.conf.urls import url
 
 from . import views
 
+# define the names of each URL parameter, and limit the format using regex
+room_param = "(?P<{param_name}>{room_regex})".format(
+	param_name="room_id",
+	room_regex="[a-zA-Z]+"
+)
+track_param = "(?P<{param_name}>{track_regex})".format(
+	param_name="track_id",
+	track_regex="[a-zA-Z0-9]+"
+)
+
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+
+	url(r'^rooms/create$', views.create_room),
+	url(r'^rooms/{room_param}$'.format(room_param=room_param), views.room_request),
+
+	url(r'^queues/{room_param}$'.format(room_param=room_param), views.read_queue),
+	url(r'^queues/{room_param}/{track_param}$'.format(room_param=room_param, track_param=track_param), views.queue_request
+	),
 ]
--- crowd_server/crowd_control/views.py
+++ crowd_server/crowd_control/views.py
@@ -1,6 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from crowd_control.models import Host, Room, Track
+
 # Create your views here.
 def index(request):
 	return HttpResponse("CrowdPlayer is currently in development.")
+
+@api_view(['POST'])
+def create_room(request):
+
+	if request.method == 'POST':
+		return Response("POST /api/rooms/create/", status=status.HTTP_501_NOT_IMPLEMENTED)
+
+@api_view(['GET', 'DELETE'])
+def room_request(request, room_id):
+
+	if request.method == 'GET':
+		return Response("GET /api/rooms/{room_id})".format(room_id=room_id), status=status.HTTP_501_NOT_IMPLEMENTED)
+
+	elif request.method == 'DELETE':
+		return Response("DELETE /api/rooms/{room_id})".format(room_id=room_id), status=status.HTTP_501_NOT_IMPLEMENTED)
+
+@api_view(['GET'])
+def read_queue(request, room_id):
+
+	if request.method == 'GET':
+		return Response("GET /api/queues/{room_id})".format(room_id=room_id), status=status.HTTP_501_NOT_IMPLEMENTED)
+
+@api_view(['POST', 'PUT', 'DELETE'])
+def queue_request(request, room_id, track_id):
+
+	if request.method == 'POST':
+		return Response("POST /api/queues/{room_id}/{track_id})".format(room_id=room_id, track_id=track_id), status=status.HTTP_501_NOT_IMPLEMENTED)
+
+	elif request.method == 'PUT':
+		return Response("PUT /api/queues/{room_id}/{track_id})".format(room_id=room_id, track_id=track_id), status=status.HTTP_501_NOT_IMPLEMENTED)
+
+	elif request.method == 'DELETE':
+		return Response("DELETE /api/queues/{room_id}/{track_id})".format(room_id=room_id, track_id=track_id), status=status.HTTP_501_NOT_IMPLEMENTED)
