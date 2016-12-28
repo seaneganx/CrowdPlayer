779aadf6eff634b756e3f93cf52faad5c579b115
Sean Egan <seaneganx@gmail.com>
2016-12-28 01:43:42 -0600

    added auth tokens to each user

--- crowd_server/crowd_control/models.py
+++ crowd_server/crowd_control/models.py
@@ -3,6 +3,15 @@ from django.db.models import Min, Max
 
 from django.utils import timezone
 
+from rest_framework.authtoken.models import Token
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+	if created:
+		Token.objects.create(user=instance)
 # Create your models here.
 class Host(models.Model):
 
