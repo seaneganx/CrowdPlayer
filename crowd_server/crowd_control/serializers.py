7b6db8714babb67077627e1aaa8c4f25a62a7a1e
Sean Egan <seaneganx@gmail.com>
2016-12-31 18:30:29 -0600

    created basic serializers for some models

---
+++ crowd_server/crowd_control/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from crowd_control import models
+from django.conf import settings
+
+class UserSerializer(serializers.Serializer):
+	username = serializers.CharField(max_length=100)
+	first_name = serializers.CharField(max_length=100)
+	last_name = serializers.CharField(max_length=100)
+	date_joined = serializers.DateTimeField()
+
+class HostSerializer(serializers.ModelSerializer):
+	user = UserSerializer(
+		read_only=True,
+	)
+
+	class Meta:
+		model = models.Host
+		fields = (
+			'user',
+			'spotify_id',
+		)
+
+class RoomSerializer(serializers.ModelSerializer):
+	host = serializers.StringRelatedField()
+
+	class Meta:
+		model = models.Room
+		fields = (
+			'host',
+			'name',
+			'creation_date',
+		)
+
+class TrackSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = models.Track
+		fields = (
+			'spotify_id',
+			'artist_name',
+			'track_name',
+			'album_name',
+			'track_length_ms',
+			'vote_count',
+			'date_added',
+		)
+
+class QueueSerializer(serializers.ModelSerializer):
+	tracks = TrackSerializer(
+		read_only=True,
+		many=True,
+	)
+
+	class Meta:
+		model = models.Room
+		fields = (
+			'tracks',
+		)
