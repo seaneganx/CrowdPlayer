cc7b694b03c951993be57c358b27714c6b0ee462
Sean Egan <seaneganx@gmail.com>
2016-12-31 18:27:29 -0600

    created host permission class allowing endpoints to require "host priviledges" (#3)

--- crowd_server/crowd_control/views.py
+++ crowd_server/crowd_control/views.py
@@ -1,8 +1,14 @@
-from rest_framework import status
+from rest_framework import status, permissions
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 import random
+
+class HostPermission(permissions.BasePermission):
+
+	def has_permission(self, request, view):
+		return hasattr(request.user, 'host')
 
 class RoomCreation(APIView):
 
