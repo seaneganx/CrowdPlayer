3a3e545f9876ce3ce56e8bf70e0cc21221a5c7ef
Sean Egan <seaneganx@gmail.com>
2017-01-26 22:23:13 -0600

    removed unused serializers

--- crowd_server/crowd_control/serializers.py
+++ crowd_server/crowd_control/serializers.py
@@ -2,24 +2,6 @@ from rest_framework import serializers
 from crowd_control import models
 from django.conf import settings
 
-class UserSerializer(serializers.Serializer):
-	username = serializers.CharField(max_length=100)
-	first_name = serializers.CharField(max_length=100)
-	last_name = serializers.CharField(max_length=100)
-	date_joined = serializers.DateTimeField()
-
-class HostSerializer(serializers.ModelSerializer):
-	user = UserSerializer(
-		read_only=True,
-	)
-
-	class Meta:
-		model = models.Host
-		fields = (
-			'user',
-			'spotify_id',
-		)
-
 class RoomSerializer(serializers.ModelSerializer):
 	host = serializers.StringRelatedField()
 
